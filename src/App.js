415d7ffaa9ac5d0a5fd6f1771b21342fafb89483
lucy-kim05 <effelt0522@naver.com>
2025-10-22 20:40:42 +0900

    feat(App): 프로그램 제어 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import Input from './view/Input.js';
+import Output from './view/Output.js';
+import Validator from './validator/Validator.js';
+import Game from './domain/Game.js';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      //입력
+      const names = await Input.readNames();
+      const count = await Input.readCount();
+
+      //검증
+      Validator.validateNames(names);
+      Validator.validateCount(count);
+
+      //게임 실행
+      const game = new Game(names, count);
+      Output.printGameStart();
+
+      for (let i = 0; i < count; i++) {
+        game.round();
+        Output.printResult(game.getCars());
+      }
+
+      //결과 출력
+      const winners = game.getWinners();
+      Output.printWinners(winners);
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
