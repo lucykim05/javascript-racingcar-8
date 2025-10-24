9ea14da68d2f6f58f2b8becbf1e6c429666ac709
lucy-kim05 <effelt0522@naver.com>
2025-10-24 16:46:30 +0900

    fix(Validator): names와 count 검증 내용 추가

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -5,13 +5,13 @@ import {
 
 class Validator {
   static validateNames(names) {
-    if (names.some((name) => name.length > 5)) {
+    if (names.some((name) => name.length === 0 || name.length > 5)) {
       throw new Error(NAME_LENGTH_ERROR);
     }
   }
 
   static validateCount(count) {
-    if (count <= 0) {
+    if (isNaN(count) || count <= 0) {
       throw new Error(COUNT_VALUE_ERROR);
     }
   }
